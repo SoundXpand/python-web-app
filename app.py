2d344b9c6fb5ccbe03c0b8bbda3048aa55ab586c
soundxpand <admin@soundxpand.com>
2023-01-31 11:41:49 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/ask", methods=["POST"])
+def ask():
+    user_question = request.form["user_question"]
+    # Use your AI model to generate a response
+    chatbot_response = "This is a response from Chatbot."
+    return render_template("index.html", user_question=user_question, chatbot_response=chatbot_response)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run()
